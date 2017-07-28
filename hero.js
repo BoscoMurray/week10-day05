840462449f29415d84994e4cc484855ce77f70ce
BoscoMurray <rossamurray@gmail.com>
2017-07-28 14:37:47 +0100

    can eat food, increase health

--- hero.js
+++ hero.js
@@ -9,5 +9,8 @@ Hero.prototype.sayName = function() {
   return "My name is " + this.name;
 }
 
+Hero.prototype.eatFood = function( food ) {
+  this.health += food === this.faveFood ? food.repValue * 1.5 : food.repValue;
+};
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,15 @@
 var assert = require("assert");
 var Hero = require("../hero");
+var Food = require("../food");
 
 describe("Hero", function () {
 
   var hero;
+  var banana;
 
   beforeEach( function() {
-    hero = new Hero("Bananaman", 10, "Banana");
+    banana = new Food("Banana", 20);
+    hero = new Hero("Bananaman", 100, banana);
   });
 
   it("should have a name", function() {
@@ -14,11 +17,11 @@ describe("Hero", function () {
   });
 
   it("should have health", function() {
-    assert.strictEqual(hero.health, 10);
+    assert.strictEqual(hero.health, 100);
   });
 
   it("should have faveFood", function() {
-    assert.strictEqual(hero.faveFood, "Banana");
+    assert.strictEqual(hero.faveFood, banana);
   });
 
   it("should talk, saying name", function() {
@@ -28,5 +31,10 @@ describe("Hero", function () {
   it("should start with empty collection of tasks", function() {
     assert.strictEqual(hero.tasks.length, 0)
   });
+
+  it("should eat food, health increases", function() {
+    hero.eatFood(banana);
+    assert.strictEqual(hero.health, 130);
+  })
 
 })

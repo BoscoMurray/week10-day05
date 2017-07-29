fc6e9c811d286db1dbee8bbc103b11efc304d5f8
BoscoMurray <rossamurray@gmail.com>
2017-07-29 08:32:20 +0100

    rat can poison food

--- rat.js
+++ rat.js
@@ -2,4 +2,8 @@ var Rat = function(name) {
   this.name = name;
 }
 
+Rat.prototype.touchFood = function( food ) {
+  food.poison();
+};
+
 module.exports = Rat;
--- specs/rat_spec.js
+++ specs/rat_spec.js
@@ -1,16 +1,24 @@
 var assert = require("assert");
 var Rat = require("../rat");
+var Food = require("../food");
 
 describe("Rat", function () {
 
   var rat;
+  var food;
 
   beforeEach( function() {
     rat = new Rat("Splinter");
+    food = new Food("Banana", 20);
   });
 
   it("should have a name", function() {
     assert.strictEqual(rat.name, "Splinter");
+  });
+
+  it("should be able to poison food", function() {
+    rat.touchFood(food);
+    assert.strictEqual(food.poisoned, true)
   });
 
 })
